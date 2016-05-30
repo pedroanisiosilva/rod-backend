e4a7cc25181fea645448e7114237184f2230115f
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-05-30 11:26:21 -0300

    Duration in seconds

--- db/migrate/20160530130957_change_duration_type_in_runs.rb
+++ db/migrate/20160530130957_change_duration_type_in_runs.rb
@@ -1,5 +1,5 @@
 class ChangeDurationTypeInRuns < ActiveRecord::Migration
   def change
-  	change_column :runs, :duration, :integer
+  	change_column :runs, :duration, 'integer USING CAST("duration" AS integer)'
   end
 end
