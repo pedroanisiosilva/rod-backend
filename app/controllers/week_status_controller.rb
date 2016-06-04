7e384d4535597a18da3713376ee88a9f776095c8
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-06-04 02:33:47 -0300

    ainda sem ordem

--- app/controllers/week_status_controller.rb
+++ app/controllers/week_status_controller.rb
@@ -1,11 +1,30 @@
+
+class WeekResults
+end
+
+
 class WeekStatusController < ApplicationController
 
   # GET /week_status
   # GET /week_status.json
-  def index
 
-    @users = User.all
 
+
+def index
+
+	categories = Hash.new
+	categories = {"Extreme Black" => "+450km", "Extreme Red" => "+350km", "Black" => "+250km",
+        "Purple" => "+150km","Blue" => "+100km", "Green" => "+75km",
+        "Orange" => "+50km", "Yellow" => "+25km", "White" => "0-24km"}
+
+    @result = Hash.new 
+
+	categories.each do |key, value|
+
+		runners= User.group('users.id','categories.id','runs.id').joins(:categories, :runs).select('*, sum(runs.distance) as total_distance').where(["categories.name = ? and runs.datetime >= ? AND runs.datetime <= ?", key ,Date.today.beginning_of_week,Date.today.end_of_week])
+
+		@result[key] = runners
+
+	end
   end
-
 end
