ecdb0898d80a39f2a55c2948ef6a20b774473f81
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-06-04 08:31:49 -0300

    ajuste de layout

--- app/controllers/week_status_controller.rb
+++ app/controllers/week_status_controller.rb
@@ -22,13 +22,8 @@ def index
 	categories.each do |key, value|
 
 		runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name').where(["categories.name = ?", key])
-
 		runners = runners.sort { |a,b| a.runs.where(:datetime => Date.today.beginning_of_week..Date.today.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..Date.today.end_of_week).sum(:distance).to_i}
 		runners = runners.reverse
-		# runners = User.joins("LEFT OUTER JOIN categories ON users.id = categories.user_id").where(["categories.name = ?", key])
-		# runners = User.joins("LEFT OUTER JOIN runs ON users.id = runs.user_id").joins(:categories).where(["categories.name = ?", key])
-		# runners = User.group('users.id').joins(:categories, :runs).select('users.id, users.name, coalesce(SUM(runs.distance),0) as total_distance').where(["categories.name = ?, key ,Date.today.beginning_of_week,Date.today.end_of_week]).order("total_distance DESC")
-
 		@result[key] = runners
 
 	end
