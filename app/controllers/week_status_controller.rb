68040f7ef97617b85d979fc884b89231e404af4b
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-06-13 09:00:16 -0300

    fix bugs

--- app/controllers/week_status_controller.rb
+++ app/controllers/week_status_controller.rb
@@ -18,34 +18,32 @@ def index
 
   @result = Hash.new
 
-  range_date = Date.today
+  if params[:week_number].nil?
+    week = Date.today.at_beginning_of_week.strftime("%U").to_i
+  else
+    week = params[:week_number]
+  end
+
+  range_date = Date.commercial(Time.now.year.to_i, week)
 
     if params[:belt]
 
-    		if params[:week_number]
-    			range_date = Date.commercial(Time.now.year.to_i, params[:week_number].to_i)
-    		end
-
-    		belt = params[:belt].capitalize
+    	belt = params[:belt].capitalize
 			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ?", belt])
-			runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..Date.today.end_of_week).sum(:distance).to_i}
+			runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
 			runners = runners.reverse
 			@result[belt] = runners    	
 
     else
 
-    if params[:week_number]
-        range_date = Date.commercial(Time.now.year.to_i, params[:week_number].to_i)
-      end
+		  categories.each do |key, value|
 
-		categories.each do |key, value|
+  			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ?", key])
+  			runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
+  			runners = runners.reverse
+  			@result[key] = runners
 
-			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ?", key])
-			runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..Date.today.end_of_week).sum(:distance).to_i}
-			runners = runners.reverse
-			@result[key] = runners
-
-		end    	
+		  end    	
     end
   end
 end
---
+++ app/jobs/weekly_targets_job.rb
@@ -0,0 +1,36 @@
+class WeeklyTargetsJob < ActiveJob::Base
+  queue_as :default
+
+	def simple_target
+
+		## same last week if above target
+		## last week *1.1 if bellow target
+		## minimum 8km
+	    tomorrow = Date.tomorrow
+	    users = User.active
+
+	    users.each do | user |
+
+		    if user.weekly_runs_km >= user.weekly_goal.distance
+		    	new_target = "%2.0f"%user.weekly_runs_km
+		    else
+		    	if user.weekly_runs_km < 8
+		    		new_target = 8
+		    	else
+		    		new_target = "%2.0f"%user.weekly_runs_km*1.1
+		    	end
+		    end   	
+
+			WeeklyGoal.create(first_day:tomorrow.at_beginning_of_week, last_day: tomorrow.at_end_of_week, number: tomorrow.at_beginning_of_week.strftime("%U").to_i, distance:new_target, user_id:user.id)
+	  	end
+	end
+
+	def perform(*args)
+    # Do something later
+		self.simple_target
+ 	end
+
+end
+
+WeeklyTargetsJob.set(wait_until: Date.today.noon).perform_later
+
