08d1c8de26935958229a6c18a2234426f9148d5e
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-06-18 15:01:30 -0300

    correct asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,5 @@ module Runordie
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += %w( login.js )
-    config.assets.precompile += %w( login-style.css )
-    config.assets.precompile += %w( login-normalized.css )
-
-
   end
 end
