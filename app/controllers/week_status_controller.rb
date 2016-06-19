b5581e661cdc92d52a695d76b5284e918a96f802
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-06-19 19:01:01 -0300

    fix previous week runners

--- app/controllers/week_status_controller.rb
+++ app/controllers/week_status_controller.rb
@@ -30,7 +30,7 @@ def index
 
     	belt = params[:belt].capitalize
 			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ?", belt])
-			self.delete_from_relation_if_no_goal(runners)
+			self.delete_from_relation_if_no_goal(runners,range_date)
       runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
 			runners = runners.reverse
 			@result[belt] = runners    	
@@ -40,7 +40,7 @@ def index
 		  categories.each do |key, value|
 
   			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ?", key])
-  			self.delete_from_relation_if_no_goal(runners)
+  			self.delete_from_relation_if_no_goal(runners,range_date)
         runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
   			runners = runners.reverse
   			@result[key] = runners
@@ -49,8 +49,8 @@ def index
     end
   end
 
-  def delete_from_relation_if_no_goal (runners)
-    runners.to_a.delete_if {|runner| runner.weekly_goal.nil?}
+  def delete_from_relation_if_no_goal (runners,date)
+    runners.to_a.delete_if {|runner| runner.weekly_goal_on_date(date).nil?}
   end
 
 end
