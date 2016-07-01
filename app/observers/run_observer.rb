45caed69c462cf8f659136bec6c3ead9136c4b0a
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-07-01 12:07:37 -0300

    show pace

--- app/observers/run_observer.rb
+++ app/observers/run_observer.rb
@@ -1,14 +1,31 @@
 class RunObserver < ActiveRecord::Observer
   attr :comunicator
-  def after_create(run)
-    comunicator.send_msg("👏👏👏 #{short_name(run.user.name)} correu #{run.distance}Km em #{Time.at(run.duration).utc.strftime("%H:%M:%S")}✔️", run.rod_images.first);
-  end
 
-  def comunicator
-    @comunicator ||= Comunicator::RodTelegram.new
-  end
 
-    def short_name(full_name)
-    	%{#{full_name.split(" ")[0]} #{full_name.split(" ")[-1]}}
-  	end
+
+	def random_message(message_array)
+		message_array[Random.rand(0..message_array.size-1)] 
+	end
+
+	  def after_create(run)
+
+	  	new_run_congrats = Array.new
+		new_run_congrats[0] = "👏👏👏 #{short_name(run.user.name)} correu #{"%3.1f"%run.distance}Km a #{run.pace} min/km✔️"
+		new_run_congrats[1] = "Congrats!🏃 #{short_name(run.user.name)} correu #{"%3.1f"%run.distance}Km a #{run.pace} min/km!️👊"
+		new_run_congrats[2] = "🏃👏👏 #{short_name(run.user.name)} correu #{"%3.1f"%run.distance}Km a #{run.pace} min/km"
+		new_run_congrats[3] = "👏🎉🏃 #{short_name(run.user.name)} correu #{"%3.1f"%run.distance}Km a #{run.pace} min/km"
+
+
+	    comunicator.send_msg(random_message(new_run_congrats), run.rod_images.first)
+	  end
+
+	  def comunicator
+	    @comunicator ||= Comunicator::RodTelegram.new
+	  end
+
+	def short_name(full_name)
+		%{#{full_name.split(" ")[0]} #{full_name.split(" ")[-1]}}
+	end
 end
+
+
