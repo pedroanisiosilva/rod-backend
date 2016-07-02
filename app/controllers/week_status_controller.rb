33e6fba7d7f900e7539a7fab49573203538b0612
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-07-02 00:56:38 -0300

    fix categories bug during change in months

--- app/controllers/week_status_controller.rb
+++ app/controllers/week_status_controller.rb
@@ -26,10 +26,16 @@ def index
 
   range_date = Date.commercial(Time.now.year.to_i, week)
 
+  if range_date.beginning_of_week.month != range_date.end_of_week.month
+    filter_date = Date.today
+  else
+    filter_date = range_date
+  end
+
     if params[:belt]
 
     	belt = params[:belt].capitalize
-			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ? and categories.first_day = ?", belt, Date.today.at_beginning_of_month])
+			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ? and categories.first_day = ?", belt, filter_date.at_beginning_of_month])
 			self.delete_from_relation_if_no_goal(runners,range_date)
       runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
 			runners = runners.reverse
@@ -39,7 +45,7 @@ def index
 
 		  categories.each do |key, value|
 
-  			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ? and categories.first_day = ?", key, Date.today.at_beginning_of_month])
+  			runners = User.group('users.id').joins(:categories).includes(:runs).select('users.id,users.name,status').where(["categories.name = ? and categories.first_day = ?", key, filter_date.at_beginning_of_month])
   			self.delete_from_relation_if_no_goal(runners,range_date)
         runners = runners.sort { |a,b| a.runs.where(:datetime => range_date.beginning_of_week..range_date.end_of_week).sum(:distance).to_i<=> b.runs.where(:datetime => Date.today.beginning_of_week..range_date.end_of_week).sum(:distance).to_i}
   			runners = runners.reverse
