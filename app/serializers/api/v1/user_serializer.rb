21de7168923455830ef92ee78e1cb4c5f21d2bda
Wilson Souza <wilson.souza@wm.com.br>
2016-07-23 12:19:05 -0300

    Add total of ran in meters

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,9 +1,14 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
-  attributes :id, :email, :name,  :status, :role, :created_at, :updated_at
+  attributes :id, :email, :name,  :status, :role, :created_at, :updated_at, :total_ran_meters
+
   has_many :runs
 
   def role
     object.role.name
+  end
+
+  def total_ran_meters
+    object.runs.sum(:distance)
   end
 
   def created_at
