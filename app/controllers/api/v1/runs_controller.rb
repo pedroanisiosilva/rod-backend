0035e85c2bea60fee15bb7f8fdc6d91310776b77
Wilson Souza <wilson.souza@wm.com.br>
2016-07-30 12:43:50 -0300

    Fix qdo não existir imagem

--- app/controllers/api/v1/runs_controller.rb
+++ app/controllers/api/v1/runs_controller.rb
@@ -40,7 +40,7 @@ class Api::V1::RunsController < Api::V1::BaseController
 
   def update
       @run = Run.find_by_id(params[:id])
-      @run.update(duration: params[:duration], distance: params[:distance], datetime: Time.parse(params[:datetime]).to_datetime, updated_at: Time.zone.now)      
+      @run.update(duration: params[:duration], distance: params[:distance], datetime: Time.parse(params[:datetime]).to_datetime, updated_at: Time.zone.now)
       if @run.save
         render(json: Api::V1::RunSerializer.new(@run).to_json)
     else
@@ -49,8 +49,8 @@ class Api::V1::RunsController < Api::V1::BaseController
   end
 
   def create
-    # byebug
-    @run = Run.create(user_id: params[:user_id], rod_images_attributes: params[:rod_images_attributes], duration: params[:duration], created_at: Time.zone.now, distance: params[:distance], datetime: Time.parse(params[:datetime]).to_datetime, updated_at: Time.zone.now)
+    byebug
+    @run = Run.create(user_id: params[:user_id], rod_images_attributes: (params[:rod_images_attributes] || []), duration: params[:duration], created_at: Time.zone.now, distance: params[:distance], datetime: Time.parse(params[:datetime]).to_datetime, updated_at: Time.zone.now)
 
 	  if @run.save
 		  render(json: Api::V1::RunSerializer.new(@run).to_json)
