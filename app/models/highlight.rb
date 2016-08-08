9c982d89111919d86eee7343b3744e6ab8bf18e4
Pedro Anisio Silva <pedroanisio@gmail.com>
2016-08-08 08:23:37 -0300

    fix hookie msg

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -40,7 +40,11 @@ class Highlight
 		freshman = @runners.where(:created_at => 2.week.ago.beginning_of_week..1.week.ago.end_of_week)
 		unless freshman.nil?
 			real = freshman.each_with_index.map{|f,i| [f.weekly_runs_km_on_date(1.week.ago.beginning_of_week),i]}
-			freshman[real.sort.last[1]]
+			unless real.nil?
+				freshman[real.sort.last[1]]
+			else
+				nil
+			end	
 		else
 			nil
 		end
